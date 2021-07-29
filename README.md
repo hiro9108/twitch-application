d78e6b309f7a93fe88c6ea9df1e9e5216f0378ea
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-07-29 13:13:51 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Twitch API Assessment
+
+> Create the `.env` file.
+
+```sh
+cp .env.example .env
+```
+
+> Please follow `xxxxx`
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1>Twitch API</h1>
+    </div>
+  );
+}
+
+export default App;
